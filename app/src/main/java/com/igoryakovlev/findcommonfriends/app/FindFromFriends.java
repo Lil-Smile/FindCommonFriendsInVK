8c12a639b283f58860b2792d4936fb092237a364
Igor <i.got.that.flower@gmail.com>
2015-09-10 13:44:15 +0300

    creating request for friends

--- app/src/main/java/com/igoryakovlev/findcommonfriends/app/FindFromFriends.java
+++ app/src/main/java/com/igoryakovlev/findcommonfriends/app/FindFromFriends.java
@@ -10,11 +10,24 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 
 
 public class FindFromFriends extends Activity implements View.OnClickListener{
+
+    private static String ITEMS = "items";
+    private static String ID = "id";
+    private static String LAST_NAME = "last_name";
+    private static String FIRST_NAME = "first_name";
+    private static String PHOTO_50 = "photo_50";
+
 
     ProgressBar progressBarMyFriendsList;
     Button buttonFindFromMyFriends;
@@ -75,7 +88,29 @@ public class FindFromFriends extends Activity implements View.OnClickListener{
     {
         @Override
         protected ArrayList<User> doInBackground(Void... voids) {
+            VKRequest request = new VKRequest("users.get", VKParameters.from(VKApiConst.FIELDS,"id,first_name,last_name,photo_50"));
+            request.executeWithListener(new VKRequest.VKRequestListener() {
+                @Override
+                public void onComplete(VKResponse response) {
+                    super.onComplete(response);
+                    JSONObject jsonObject = response.json;
+                    ArrayList<User> data = new ArrayList<User>();
+                    try {
+                        JSONArray jsonArray = jsonObject.getJSONArray(ITEMS);
+                        for (int i = 0; i<jsonArray.length(); i++)
+                        {
+                            JSONObject tmp = jsonArray.getJSONObject(i);
+                            String name = tmp.getString(FIRST_NAME);
+                            String surname = tmp.getString(LAST_NAME);
+                            String id = tmp.getString(ID);
+                            String photo_50StringURL = tmp.getString(PHOTO_50);
 
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
             return null;
         }
 
